56335247568ca1b1f2eca9dd2a1ccbed310c8e8e
PatriciaMG92 <xiqitinah_92@hotmail.com>
2023-04-25 11:01:06 +0000

    added navigate in navbar vertical

--- src/front/js/pages/userAcountModifyUser.js
+++ src/front/js/pages/userAcountModifyUser.js
@@ -4,10 +4,21 @@ import NavbarVertical from "../component/navBarVertical";
 
 export const UserAcountModifyUser = () => {
 
+	const colStyle={
+		width: 20 + 'rem',
+		height: 23 + 'rem',
+		padding: 0
+	}
 	return (
-		<div>
-			<NavbarVertical />
-			<ModifyUser />
+		<div className="container m-0">
+			<div className="row">
+				<div className="col-3 border rounded border-warning p-0" style={colStyle}>
+					<NavbarVertical />
+				</div>
+				<div className="col-9 pt-5 justify-content-center">
+					<ModifyUser />
+				</div>
+			</div>
 		</div>
 	);
 };
