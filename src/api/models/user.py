f772a525fe5bca05fe76d5135df5d4aa65dc3390
Alefantozzi86 <119671613+Alefantozzi86@users.noreply.github.com>
2023-04-26 17:26:16 +0000

    db user message

--- src/api/models/user.py
+++ src/api/models/user.py
@@ -1,11 +1,31 @@
 from api.models.db import db
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
 
 class User(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120), unique=True, nullable=False)
+    name = db.Column(db.String(120), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+
+class Message(db.Model):
+    __tablename__ = 'message'
+    id = Column(Integer, primary_key=True)
+    comment_text = Column(String(250))
+    author_id = Column(Integer, ForeignKey('user.id'))
+    post_id = Column(Integer, ForeignKey('post.id'))
+    
+    author = relationship("User", backref="messages")
+    
+    post = relationship("Post", backref="messages")
+
+class Post(db.Model):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+       
+    user = relationship("User", backref="posts")
 
     def __repr__(self):
         return f'<User {self.email}>'
