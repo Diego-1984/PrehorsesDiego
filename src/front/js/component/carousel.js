50bf0be02c5b96de44a9ebda10a943b00681aef6
PatriciaMG92 <xiqitinah_92@hotmail.com>
2023-05-05 09:40:04 +0000

    cards funcionando y navbar bien

--- src/front/js/component/carousel.js
+++ src/front/js/component/carousel.js
@@ -7,25 +7,95 @@ const Carousel = () =>{
 
     return (
         <>
-        <div className="row">
-            <div className="col-12 m-4">
-                <Link to="/male">
-                    <img src={caballoMacho} />
-			    </Link>
-            </div>
-            <div className="col-12 m-4">
-                <Link to="/mares">
-                    <img src={caballosHembras} />
-			    </Link>
-            </div>
-            <div className="col-12 m-4">
-                <Link to="/gelding">
-                    <img src={caballoCastrado} />
-			    </Link>
-            </div>
-            </div>
+        <div className="col-12 m-15">
+            <div className="container1 d-flex align-items-center justify-content-center flex-wrap">
+                <div className="box">
+                    <div className="body">
+                        <div className="imgContainer">
+                            <img src={caballoMacho} />
+                        </div>
+                        <div className="content d-flex flex-column align-items-center justify-content-center">
+                            <div>
+                                <h3 className="text-white fs-8">Caballos Macho</h3>
+                                <p>
+                                    En esta sección podras descubrir todos los caballos macho
+                                    disponibles y poder ofertar con su dueño.
+                                </p>
+                                <Link to="/male">
+                                    <button type="button" class="btn btn-outline-secondary">Ver Caballos Machos</button>
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>  
+            <div className="container1 d-flex align-items-center justify-content-center flex-wrap">
+                <div className="box">
+                    <div className="body">
+                        <div className="imgContainer">
+                            <img src={caballosHembras} />
+                        </div>
+                        <div className="content d-flex flex-column align-items-center justify-content-center">
+                            <div>
+                                <h3 className="text-white fs-8">Hembras</h3>
+                                <p>
+                                    En esta sección podras descubrir todas las Yeguas
+                                    disponibles y poder ofertar con su dueño.
+                                </p>
+                                <Link to="/male">
+                                    <button type="button" class="btn btn-outline-secondary">Ver Caballos Machos</button>
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>  
+            <div className="container1 d-flex align-items-center justify-content-center flex-wrap">
+                <div className="box">
+                    <div className="body">
+                        <div className="imgContainer">
+                            <img src={caballoCastrado} />
+                        </div>
+                        <div className="content d-flex flex-column align-items-center justify-content-center">
+                            <div>
+                                <h3 className="text-white fs-8">Caballos Castrados</h3>
+                                <p>
+                                    En esta sección podras descubrir todos los caballos castrados
+                                    disponibles y poder ofertar con su dueño.
+                                </p>
+                                <Link to="/male">
+                                    <button type="button" class="btn btn-outline-secondary">Ver Caballos Machos</button>
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>    
+        </div>
         </>
-    )
-}
+      );
+    };
+//     return (
+//         <>
+//         <div className="row">
+//             <div className="col-12 m-4">
+//                 <Link to="/male">
+//                     <img src={caballoMacho} />
+// 			    </Link>
+//             </div>
+//             <div className="col-12 m-4">
+//                 <Link to="/mares">
+//                     <img src={caballosHembras} />
+// 			    </Link>
+//             </div>
+//             <div className="col-12 m-4">
+//                 <Link to="/gelding">
+//                     <img src={caballoCastrado} />
+// 			    </Link>
+//             </div>
+//             </div>
+//         </>
+//     )
+// }
 
 export default Carousel;
