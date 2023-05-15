ef8b63485e05eb25cd09e0a04d891b8606c4c9ce
PatriciaMG92 <xiqitinah_92@hotmail.com>
2023-05-15 10:52:10 +0000

    ver mensajes dentro de su caja

--- src/front/js/component/chat.js
+++ src/front/js/component/chat.js
@@ -27,13 +27,6 @@ const Chat = ({horse}) => {
   return (
     <>
       <div className="container text-center messagesBox">
-        {<div>
-            <div>{store.messages.map((item)=>{
-              return (
-                <p>{item.text}</p>
-              )
-           })}</div>
-          </div>}
         <div className="row borber border-bottom messagesTitle">
           <div className="col p-3 text-start">
             <span className="material-symbols-outlined align-center me-2">
@@ -45,6 +38,13 @@ const Chat = ({horse}) => {
           </div>
         </div>
         <div className="row mt-3 messages">
+        {<div>
+            <div>{store.messages.map((item)=>{
+              return (
+                <p>{item.text}</p>
+              )
+           })}</div>
+          </div>}
         </div>
         <div className="row messageToSend align-items-center border-top">
           <div className="col justify-content-end mt-3 me-2">
