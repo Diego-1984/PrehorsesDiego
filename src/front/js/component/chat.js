52f61642c2c48f51ce1cfd08b1491b22da7b69f5
Diego-1984 <diegomenoruceta@gmail.com>
2023-06-22 19:08:02 +0000

    Chat en vivo pendiente de comprobar

--- src/front/js/component/chat.js
+++ src/front/js/component/chat.js
@@ -1,15 +1,35 @@
-import React, { useState } from "react";
-import { Loader } from '../component/loader';
-const Chat = ({horse, messages, postMessage, userInterestedId}) => {
+import React, { useState, useEffect } from "react";
+import { Loader } from "../component/loader";
+const Chat = ({ horse, messages, postMessage, userInterestedId }) => {
   const [message, setMessage] = useState([]);
   const [loading, setLoading] = useState(false);
   const [messageInput, setMessageInput] = useState();
-  const sendMessage = async(horse, message, userInterestedId) => {
+  const sendMessage = async (horse, message, userInterestedId) => {
     setLoading(true);
-    messageInput.value = '';
+    messageInput.value = "";
     await postMessage(horse, message, userInterestedId);
     setLoading(false);
-  }
+  };
+
+  //intento de chat en vivo
+
+  useEffect(() => {
+    const fetchMessages = async () => {
+      const response = await fetch(
+        "https://3001-4geeksacade-pt36horsesp-fcsdk344w3o.ws-eu100.gitpod.io/admin/message/"
+      );
+      const data = await response.json();
+      setMessage(data);
+    };
+
+    fetchMessages();
+    const intervalId = setInterval(fetchMessages, 3000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
   return (
     <>
       <div className="container text-center messagesBox">
@@ -19,24 +39,27 @@ const Chat = ({horse, messages, postMessage, userInterestedId}) => {
               person
             </span>
             <span className="align-center">
-              {!!userInterestedId ? messages[0].userOwner.name : horse.userOwner.name}
+              {!!userInterestedId
+                ? messages[0].userOwner.name
+                : horse.userOwner.name}
             </span>
           </div>
         </div>
         <div className="row mt-3 messages justify-content-start">
           <div className="d-flex justify-content-start align-items-stretch flex-column w-100">
-            {messages.map((item)=>{
-                if(item.senderId === item.userOwnerId ){
-                  return (
-                    <>
-                      <div className="w-50 owner mb-2">{item.text}</div>
-                    </>
-                  )
-                }else return(
+            {messages.map((item) => {
+              if (item.senderId === item.userOwnerId) {
+                return (
+                  <>
+                    <div className="w-50 owner mb-2">{item.text}</div>
+                  </>
+                );
+              } else
+                return (
                   <>
                     <div className="w-50 interested mb-2">{item.text}</div>
                   </>
-                )
+                );
             })}
             {loading && <Loader />}
           </div>
@@ -45,10 +68,21 @@ const Chat = ({horse, messages, postMessage, userInterestedId}) => {
           <div className="col justify-content-end mt-3 me-2">
             <div className="row ">
               <div className="col-11 p-0">
-                <input type="message" className="aling-items-center "onChange={(e)=>{setMessage({...message, text: e.target.value}); setMessageInput(e.target);}}></input>
+                <input
+                  type="message"
+                  className="aling-items-center "
+                  onChange={(e) => {
+                    setMessage({ ...message, text: e.target.value });
+                    setMessageInput(e.target);
+                  }}
+                ></input>
               </div>
               <div className="col-1 p-0">
-                <span className="material-symbols-outlined send" id="send" onClick={()=>sendMessage(horse, message, userInterestedId)}>
+                <span
+                  className="material-symbols-outlined send"
+                  id="send"
+                  onClick={() => sendMessage(horse, message, userInterestedId)}
+                >
                   send
                 </span>
               </div>
@@ -60,29 +94,3 @@ const Chat = ({horse, messages, postMessage, userInterestedId}) => {
   );
 };
 export default Chat;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
