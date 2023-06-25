379a69884f81246cdae218d6db0492095bd98816
Diego-1984 <diegomenoruceta@gmail.com>
2023-06-25 09:01:37 +0000

    Chat en vivo interested funcionando

--- src/front/js/pages/descripcionCaballo.js
+++ src/front/js/pages/descripcionCaballo.js
@@ -4,15 +4,12 @@ import { Context } from "../store/appContext";
 import { useParams } from "react-router-dom";
 import ButtonContact from "../component/buttonContact";
 import { Link } from "react-router-dom";
-
 const DescripcionCaballo = () => {
   const params = useParams();
   const { id } = params;
   const { store, actions } = useContext(Context);
   const [actualPage, setActualPage] = useState();
-
   const [isChatShown, setIsChatShown] = useState(false);
-
   const getDate = (now) => {
     var date = now.getDate();
     var month = now.getMonth() + 1;
@@ -20,16 +17,20 @@ const DescripcionCaballo = () => {
     var hours = now.getHours();
     var min = now.getMinutes();
     var sec = now.getSeconds();
-    return year + '/' + month + '/' + date + 'T' + hours + ':' + min + ':' + sec;
-  }
-
-  const postMessage = async(horse, message) =>{
-    const {id: horseId, user_id: userOwnerId} = horse
-    await actions.postMessage(message.text, horseId, userOwnerId, getDate(new Date()))
-    await actions.getMessages(horseId)
-  }
-
-  
+    return (
+      year + "/" + month + "/" + date + "T" + hours + ":" + min + ":" + sec
+    );
+  };
+  const postMessage = async (horse, message) => {
+    const { id: horseId, user_id: userOwnerId } = horse;
+    await actions.postMessage(
+      message.text,
+      horseId,
+      userOwnerId,
+      getDate(new Date())
+    );
+    await actions.getMessages(horseId);
+  };
   const datosCaballoSinChat = {
     width: 100 + "%",
   };
@@ -38,27 +39,29 @@ const DescripcionCaballo = () => {
     const actualInfo = store.horses.find((item) => item.id == id);
     setActualPage(actualInfo);
   }, [params]);
-
   useEffect(() => {
+    const updateMessages = () => {
+      actions.getMessages(id);
+    };
     actions.clearMessages();
-    actions.getMessages(id);
+    updateMessages();
+    const intervalId = setInterval(updateMessages, 3000);
+    return () => {
+      clearInterval(intervalId);
+    };
   }, []);
-
   const horse = store.horses.find((item) => item.id == id);
-
   return (
     <>
-    <div className="row mx-2 my-2">
-          <Link to="/">
-            <button className="bg-transparent border border-0">Home / Caballos / Descripción
-            </button>
-          </Link>
-        </div>
+      <div className="row mx-2 my-2">
+        <Link to="/">
+          <button className="bg-transparent border border-0">
+            Home / Caballos / Descripción
+          </button>
+        </Link>
+      </div>
       <div className="container oneHorse">
-        <div
-          className="datosCaballo"
-          style={datosCaballoSinChat}
-        >
+        <div className="datosCaballo" style={datosCaballoSinChat}>
           <div className="card mt-3 border-0">
             <div className="row g-0">
               <div className="col-md-6 w-50">
@@ -94,15 +97,11 @@ const DescripcionCaballo = () => {
             <div className="row">
               <div className="col mt-3">
                 <div className="d-flex descriptionIcons w-50">
-                  <span className="material-symbols-outlined me-3">
-                    badge
-                  </span>
+                  <span className="material-symbols-outlined me-3">badge</span>
                   <p>{actualPage?.nombre}</p>
                 </div>
                 <div className="d-flex descriptionIcons w-50">
-                  <span className="material-symbols-outlined me-3">
-                    cake
-                  </span>
+                  <span className="material-symbols-outlined me-3">cake</span>
                   <p>{actualPage?.fecha_nacimiento}</p>
                 </div>
               </div>
@@ -114,9 +113,7 @@ const DescripcionCaballo = () => {
                   <p>{actualPage?.sexo}</p>
                 </div>
                 <div className="d-flex w-50 descriptionIcons">
-                  <span className="material-symbols-outlined me-3">
-                    map
-                  </span>
+                  <span className="material-symbols-outlined me-3">map</span>
                   <p>{actualPage?.provincia}</p>
                 </div>
               </div>
@@ -128,9 +125,7 @@ const DescripcionCaballo = () => {
                   <p>{actualPage?.capa}</p>
                 </div>
                 <div className="d-flex descriptionIcons">
-                  <span className="material-symbols-outlined me-3">
-                    height
-                  </span>
+                  <span className="material-symbols-outlined me-3">height</span>
                   <p>{actualPage?.alzada}</p>
                 </div>
               </div>
@@ -142,9 +137,7 @@ const DescripcionCaballo = () => {
                   <p>{actualPage?.nivel_doma}</p>
                 </div>
                 <div className="d-flex descriptionIcons">
-                  <span className="material-symbols-outlined me-3">
-                    fence
-                  </span>
+                  <span className="material-symbols-outlined me-3">fence</span>
                   <p>{actualPage?.ganaderia}</p>
                 </div>
               </div>
@@ -152,18 +145,27 @@ const DescripcionCaballo = () => {
           </div>
         </div>
         <div className="chat">
-          {isChatShown ? <Chat horse={horse} messages={store.messages} postMessage={postMessage}/> : null}
-          {isChatShown && <button className="chat_close-button" onClick={() => setIsChatShown(false)}>X</button>}
+          {isChatShown ? (
+            <Chat
+              horse={horse}
+              messages={store.messages}
+              postMessage={postMessage}
+            />
+          ) : null}
+          {isChatShown && (
+            <button
+              className="chat_close-button"
+              onClick={() => setIsChatShown(false)}
+            >
+              X
+            </button>
+          )}
         </div>
       </div>
     </>
   );
 };
-
 export default DescripcionCaballo;
-
-
-
 
 
 
